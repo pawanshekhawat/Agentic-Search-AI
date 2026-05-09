61bdd6c85989c49c6bf79953c63ac9a083fb6cb2
Pawan Shekhawat <shekhawatpawan2402@gmail.com>
2026-05-10 01:39:39 +0530

    feat: add Prisma backend foundation, expand Atreus API scaffolding, and add source streaming

--- backend/index.ts
+++ backend/index.ts
@@ -4,20 +4,49 @@ import { Output, streamText } from "ai";
 import { google } from '@ai-sdk/google';
 import { SYSTEM_PROMPT, PROMPT_TEMPLATE } from "./promt";
 import z from "zod";
+import { prisma } from "./db";
 
 const client = tavily({ apiKey: process.env.TAVILY_API_KEY });
 const app = express();
 
 app.use(express.json());
 
-app.post("/atreus_ask", async (req, res) => {
-    const query = req.body.query;
+// Sinup
+app.post("/signup", async (req, res) => {
 
+})
+
+//Signin
+app.post("/signin", async (req, res) => {
+
+})
+
+// Past conversations get
+app.get("/conversations", async (req, res) => {
+
+})
+
+// Past conversation get by id
+app.get("/conversations/:id", async (req, res) => {
+
+})
+
+app.post("/atreus_ask", async (req, res) => {
+    //Step 1: Get the query from the user
+    const query = req.body.query;
+    //Step 2: make sure user has access/credits to hit the endpoint
+
+    //Step 3: check if we have web search indexed for similar queries
+
+    //Step 4: if no then use web search to gather sources
     const webSearchResponse = await client.search(query, {
         searchDepth: "advanced"
     });
 
     const webSearchResults = webSearchResponse.results;
+    //Step 5: do some context engineering on the prompt + web search responses
+
+    //Step 6: hit the LLM and stream back the response
 
     const prompt = PROMPT_TEMPLATE.replace("{{WEB_SEARCH_RESULTS}}", JSON.stringify(webSearchResponse))
         .replace("{{USER_QUERY}}", query);
@@ -41,12 +70,20 @@ app.post("/atreus_ask", async (req, res) => {
     }
     res.write("\n<SOURCES>\n")
 
+    //Step 7: also stream back the sources and the follow up questions (which we can get from another parellel LLM call)
     res.write(JSON.stringify(webSearchResults.map(result => ({ url: result.url }))));
 
+    //Step 8: Close the event stream
     res.write("\n</SOURCES>\n");
     res.end();
 })
 
+app.post("/atreus_ask/followups", async (req, res) => {
+    //Step 1: get the existing chat from the db
+    //Step 2: forawrd the full history to the LLM
+    //Step 2.5: TODO: Do context engineering here.
+    //Step 3: stream the response back to the user 
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
